0d2d8677be14059c4c48709858a714aade082894
joeynhieu <jnhieu@hawaii.edu>
2022-02-16 00:36:47 -1000

    commit

---
+++ MathCalculations/math.java
@@ -0,0 +1,22 @@
+public class math {
+    
+    public static void main(String[] args) {
+        int s = 0;
+
+        int x = 1;
+    
+        while (x <= 3) {
+    
+            s = s + x;                 
+    
+            x = x + 1; 
+    
+            
+      }     
+        System.out.println(s);
+        
+        System.out.println(x);
+            
+      
+    }
+}
