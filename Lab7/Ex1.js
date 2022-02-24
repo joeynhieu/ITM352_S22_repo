93065909d8e1768388e89c4f78904eac86fa77ac
joeynhieu <jnhieu@hawaii.edu>
2022-02-24 08:39:45 -1000

    commit

--- Lab7/Ex1.js
+++ Lab7/Ex1.js
@@ -1,15 +1,13 @@
 require("./products_data.js");
 
 var num_products = 5;
-var num_counter = 1;
-while(num_counter <= num_products){
-    console.log(`${num_counter}. ${eval(`name`+ num_counter)}`);
-    num_counter++;
-    if(num_counter > (num_products/2)){
-        console.log("That's enough!");
-        break;
+for(num_counter = 1; eval("typeof name"+num_counter) != 'undefined'; num_counter++){
+   
+   if(num_counter > num_products/2){
+        //console.log("Don't ask for anything else!");
+        //process.exit();
     }
-
+        console.log(`${num_counter}. ${eval(`name`+ num_counter)}`);
     
 }
 
