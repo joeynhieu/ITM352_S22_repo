61155129272babd20063ca34269fe4315bc63e7c
joeynhieu <jnhieu@hawaii.edu>
2022-03-10 10:57:47 -1000

    commit

--- Lab10/Ex.js
+++ Lab10/Ex.js
@@ -6,18 +6,15 @@ year = 2001;
 
 
 step1 = year - 2000;
-console.log(step1);
 step2 = parseInt(step1/4);
-console.log(step2);
 step3 = step2 + step1;
-console.log(step3);
 step4 = 5;
 step6 = step4 + step3;
-console.log(step6);
 step7 = day + step6;
-console.log(step7);
 step8 = step7 - 1;
 step9 = step8 % 7;
 
 //returns the result of step 9 ==> 0-6: days of the week
 console.log(step9);
+//returns 6 --> saturday [correct day according to google]
+
