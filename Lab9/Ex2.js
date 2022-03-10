0d6ef5f930b65ccf901b8a9322c9fc3e0f086087
joeynhieu <jnhieu@hawaii.edu>
2022-03-10 02:22:55 -1000

    commit

--- Lab9/Ex2.js
+++ Lab9/Ex2.js
@@ -25,14 +25,7 @@ errors = []; // assume no errors at first
 if(Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
 if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
 if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
-if (q>0) errors.push('Non Negative Int')
-if (errors.length == 0)
-    return true;
-else
-    return false;
-
-}
-//returnErrors ? errors : (errors.length == 0)}
+returnErrors ? errors : (errors.length == 0)}
 
 
 for(piece in pieces){
