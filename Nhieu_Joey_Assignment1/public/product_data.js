5f8792a7abdd00ae135d9e4e8d8038a5e108e906
joeynhieu <jnhieu@hawaii.edu>
2022-03-29 19:53:47 -1000

    commit

---
+++ Nhieu_Joey_Assignment1/public/product_data.js
@@ -0,0 +1,32 @@
+products = 
+[
+        {
+        "brand" : "Sanrio",
+        "price" : 25.15,
+        "image" : "https://i.etsystatic.com/14420193/r/il/114385/3606877892/il_1588xN.3606877892_627y.jpg"
+        },
+        {
+        "brand" : "Studio Ghibli",
+        "price" : 22.60,
+        "image" : "https://i.etsystatic.com/32986639/r/il/785cb1/3485887490/il_1588xN.3485887490_hhkm.jpg"
+        },
+        {
+        "brand" : "Floral",
+        "price" : 15.00,
+        "image" : "https://i.etsystatic.com/23097501/r/il/c169a6/3617257273/il_1588xN.3617257273_bj66.jpg"
+        },
+        {
+        "brand" : "Koi",
+        "price" : 35.50,
+        "image" : "https://i.etsystatic.com/30971482/r/il/40f2e3/3647427635/il_1588xN.3647427635_akis.jpg"
+        },
+        {
+        "brand" : "Pokemon",
+        "price" : 50.00,
+        "image" : "https://i.etsystatic.com/26055747/r/il/e14c92/3464508854/il_1588xN.3464508854_7dns.jpg"
+        }
+    ]
+
+if(typeof module != 'undefined') {
+  module.exports.products = products;
+}
