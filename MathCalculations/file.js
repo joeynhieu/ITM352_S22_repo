63368cddde87c1415b5874d638984175cd97b35c
joeynhieu <jnhieu@hawaii.edu>
2022-04-01 23:10:18 -1000

    assignment1

---
+++ MathCalculations/file.js
@@ -0,0 +1,8 @@
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  //Open a file on the server and return its content:
+  fs.readFile('demofile1.html', function(err, data) {
+    console.log
+  });
+}).listen(8080);
--- Nhieu_Joey_Assignment1/server.js
+++ Nhieu_Joey_Assignment1/server.js
@@ -1,51 +1,62 @@
-//Coded taken and modified from Kazman's info_server_Ex4.js from lab 13
+/*Author Joey Nhieu Assignment 1 Server.js: 
+code referenced from previous lab12 
+Received help/assistance from Tai T, Joshua R*/
+
 var express = require('express');
 var app = express();
-var myParser = require("body-parser");
+//var myParser = require("body-parser");
 var data = require('./public/product_data.js');
 var products = data.products;
-var fs = require('fs');
-const queryString = require('url');
+const queryString = require('query-string');
 
 
 app.use(express.static('./public'));
-app.use(myParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.post("/process_quantity_form", function (request, response) {
     let POST = request.body;
-    
-    if (typeof POST['submitPurchase'] != 'undefined') {
-                var hasvalidquantities=true; // assuming that the variable will be true// 
-                var hasquantities=false
+  
+      // assumes that the quantity will be valid
+                var has_valid_quantities=true; 
+      // assumes that there is no quantity
+                var has_quantities=false
+
                 for (i = 0; i < products.length; i++) {
                     
                                 qty=POST[`quantity${i}`];
-                                hasquantities=hasquantities || qty>0; // If its value is larger than 0 then it is good//
-                                hasvalidquantities=hasvalidquantities && isNonNegInt(qty);    // if it is both a quantity over 0 and is valid//     
-                } 
+
+                                has_quantities = has_quantities || qty>0; // If its value is larger than 0 then it is good//
+                                
+                                has_valid_quantities = has_valid_quantities && isNonNegInt(qty);    // if it is both a quantity over 0 and is valid//     
+
+                                if (products[i].quantity > products[i].stock){
+                                    has_valid_quantities = false;
+                                }
+                }
+                
                 // if all quantities are valid, generate the invoice// 
-                const stringified = queryString.URLSearchParams;
+                const stringified = queryString.stringify(POST);
                 if (hasvalidquantities && hasquantities) {
                     console.log(stringified);
                     response.redirect("./invoice.html?"+stringified); // using the invoice.html and all the data that is input//
                 }  
-                else {response.send('Enter a valid quantity!')} 
+                // directs user to another page prompting to go back and entire valid quantity
+                else {response.send('Please go back and enter a valid quantity!')} 
             }
-        });
+        );
 
-function isNonNegInt(stringToCheck, returnErrors = false) {
-    errors = []; // assume no errors at first
-    if(stringToCheck ==""){stringToCheck = 0;}
-    if (Number(stringToCheck) != stringToCheck) errors.push('Not a number!'); // Check if string is a number value
-    if (stringToCheck < 0) errors.push('Negative value!'); // Check if it is non-negative
-    if (parseInt(stringToCheck) != stringToCheck) errors.push('Not an integer!'); // Check that it is an integer
+        function isNonNegInt(stringToCheck, returnErrors = false) {
+            errors = []; // assume no errors at first
+            if(stringToCheck ==""){stringToCheck = 0;}
+            if (Number(stringToCheck) != stringToCheck) errors.push('Not a number!'); // Check if string is a number value
+            if (stringToCheck < 0) errors.push('Negative value!'); // Check if it is non-negative
+            if (parseInt(stringToCheck) != stringToCheck) errors.push('Not an integer!'); // Check that it is an integer
+        
+            return returnErrors ? errors : (errors.length == 0);
+        }
+        
 
-    return returnErrors ? errors : (errors.length == 0);
-}
 
-app.all('*', function (request, response, next) {
-    console.log(request.method + ' to ' + request.path);
-    next();
-});
-
+/* Start server */
 app.listen(8080, () => console.log(`listening on port 8080`));
+
