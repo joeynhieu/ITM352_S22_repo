2c9864cabc9b6dd6e023d406113b9373be9bf6e9
joeynhieu <jnhieu@hawaii.edu>
2022-04-28 09:02:34 -1000

    commit

--- Nhieu_Joey_Assignment1/server.js
+++ Nhieu_Joey_Assignment1/server.js
@@ -4,46 +4,173 @@ Received help/assistance from Tai T, Joshua R*/
 
 var express = require('express');
 var app = express();
-//var myParser = require("body-parser");
+var myParser = require("body-parser");
 var data = require('./public/product_data.js');
 var products = data.products;
+var fs = require('fs');
 const queryString = require('query-string');
-
 
 app.use(express.static('./public'));
 app.use(express.urlencoded({ extended: true }));
 
+//Defines file in variable for later usage
+var filename = 'user_data.json';
+app.all('*', function(request, response, next){
+    console.log(request.method + ' to ' + request.path);
+    next();
+});
+
+//Checks for file
+if (fs.existsSync(filename)) {
+    stats = fs.statSync(filename);
+    console.log(`user_data.json has ${stats['size']} characters`);
+    var data = fs.readFileSync(filename, 'utf-8');
+    var users_reg_data = JSON.parse(data);
+} else {
+    console.log (`Error: ${Filename} does not exist.`);
+}
+
+//Sends data to invoice
+app.post("/process_form", function(request, response, next){
+   
+    //Validates data
+    var valid_qty = true; //Check for valid input. 
+    var total_purchases = false; 
+    for (i = 0; i < products.length; i++) {
+        aqty = request.body[`quantity${i}`];
+        if(isNonNegIntString(aqty) == false){
+            valid_qty &= false; //Invalid data 
+    
+        }
+        if (aqty > 0){ //Checks for visible data
+            total_purchases = true;
+        }
+    }
+    
+        // Following code taken from Brittney's code shown in class
+        purchase_qs = qs.stringify(request.body); 
+        if (valid_qty == true && total_purchases == true) { 
+            response.redirect('./login_page.html?' + purchase_qs); 
+        }
+        else { 
+            response.redirect("./products_display.html?"); 
+        }
+    });
+ 
+ 
+ //Processes user login, code taken and expanded upon from lab 14 and assignment 2 examples
+ app.post("/process_login", function (req, res) {
+     var LogError = [];
+     console.log(req.body);
+     the_username = req.body.username.toLowerCase(); //Formatting of username to fit specifications
+     if (typeof users_reg_data[the_username] != 'undefined') { 
+         if (req.body.password == users_reg_data[req.body.username].password) {
+             res.redirect('/invoice.html?' + qs.stringify(req.query) + qs.stringify(req.body.username));
+             //This redirects to the invoice if the appropriate password is entered
+         } else { //Wrong password
+             LogError.push = ('Invalid Password');
+             console.log(LogError);
+             req.query.username= the_username;
+             req.query.name= users_reg_data[the_username].name;
+             req.query.LogError=LogError.join(';');
+         }
+         } else { //States invalid username and redirects
+             LogError.push = ('Invalid Username');
+             console.log(LogError);
+             req.query.username= the_username;
+             req.query.LogError=LogError.join(';');
+         }
+     res.redirect('./login_page.html?' + qs.stringify(req.query));
+ });
+ 
+ //To create code on server side
+ //Worked on code with Philip
+ app.post("/process_registration", function (req, res) {
+     qstr = req.body;
+     console.log(qstr);
+     var errors = [];
+ 
+     if (/^[A-Za-z]+$/.test(req.body.name)) { //Allows for only letters
+     }
+     else {
+       errors.push('Use Only Letters for Full Name');
+     }
+     // Checks for full name
+     if (req.body.name == "") {
+       errors.push('Invalid Full Name');
+     }
+     // Need length of full name, fix in order to make it fit
+   if ((req.body.fullname.length > 25 && req.body.fullname.length <0)) {
+     errors.push('Full Name Too Long');
+   }
+   //checks new username
+     var reguser = req.body.username.toLowerCase(); 
+     if (typeof users_reg_data[reguser] != 'undefined') { //Gives error
+       errors.push('Username taken');
+     }
+     //Makes code the username to only be betters and numbers
+     if (/^[0-9a-zA-Z]+$/.test(req.body.username)) {
+     }
+     else {
+       errors.push('Letters And Numbers Only for Username');
+     }
+   
+     //password needs to be 6 characters
+     if (req.body.password.length < 8) {
+       errors.push('Password Too Short');
+     }
+     // matches passwords and checks
+     if (req.body.password !== req.body.repeat_password) { 
+       errors.push('Password Not a Match');
+     }
+     //Shows error, code taken directly from code 14
+     if (errors.length == 0) {
+       POST = req.body;
+       console.log('no errors');
+       var username = POST['username'];
+       users_reg_data[username] = {}; 
+       users_reg_data[username].name = username;
+       users_reg_data[username].password= POST['password'];
+       users_reg_data[username].email = POST['email'];
+       data = JSON.stringify(users_reg_data); 
+       fs.writeFileSync(filename, data, "utf-8");
+       res.redirect('./invoice4.html?' + qs.stringify(req.query));
+     }
+     e
+     if (errors.length > 0) {
+         console.log(errors);
+         req.query.name = req.body.name;
+         req.query.username = req.body.username;
+         req.query.password = req.body.password;
+         req.query.repeat_password = req.body.repeat_password;
+         req.query.email = req.body.email;
+ 
+         req.query.errors = errors.join(';');
+         res.redirect('registration.html?' + qs.stringify(req.query));
+     }
+ });
+
 app.post("/process_quantity_form", function (request, response) {
     let POST = request.body;
-  
-      // assumes that the quantity will be valid
-                var has_valid_quantities=true; 
-      // assumes that there is no quantity
+    
+    if (typeof POST['submitPurchase'] != 'undefined') {
+                var has_valid_quantities=true; // assuming that the variable will be true// 
                 var has_quantities=false
-
                 for (i = 0; i < products.length; i++) {
                     
                                 qty=POST[`quantity${i}`];
-
-                                has_quantities = has_quantities || qty>0; // If its value is larger than 0 then it is good//
-                                
-                                has_valid_quantities = has_valid_quantities && isNonNegInt(qty);    // if it is both a quantity over 0 and is valid//     
-
-                                if (products[i].quantity > products[i].stock){
-                                    has_valid_quantities = false;
-                                }
-                }
-                
+                                has_quantities=has_quantities || qty>0; // If its value is larger than 0 then it is good//
+                                has_valid_quantities=has_valid_quantities && isNonNegInt(qty);    // if it is both a quantity over 0 and is valid//     
+                } 
                 // if all quantities are valid, generate the invoice// 
                 const stringified = queryString.stringify(POST);
                 if (has_valid_quantities && has_quantities) {
-                    console.log(stringified);
                     response.redirect("./invoice.html?"+stringified); // using the invoice.html and all the data that is input//
+                    users_reg_data[stock_avl] = qty - users_reg_data[stock_avl];
                 }  
-                // directs user to another page prompting to go back and entire valid quantity
-                else {response.send('Please go back and enter a valid quantity!')} 
+                else {response.send('Enter a valid quantity!')} 
             }
-        );
+        });
 
         function isNonNegInt(stringToCheck, returnErrors = false) {
             errors = []; // assume no errors at first
