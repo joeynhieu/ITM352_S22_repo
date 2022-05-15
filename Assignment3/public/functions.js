ed79eb04dd2ed41b454390ae9d014e3b9f00743b
joeynhieu <jnhieu@hawaii.edu>
2022-05-15 11:52:53 -1000

    com

--- Assignment3/public/functions.js
+++ Assignment3/public/functions.js
@@ -12,10 +12,10 @@ function loadJSON(service, callback) {
     xobj.send(null);  
  }
 
- function nav_bar(this_product_key, products_data) {
+function nav_bar(this_product_key, products_data) {
     // This makes a navigation bar to other product pages
     for (let products_key in products_data) {
         if (products_key == this_product_key) continue;
-        document.write(`<a href='./products_display.html?products_key=${products_key}'>${products_key}<a>&nbsp&nbsp&nbsp;`);
+        document.write(`<a href='./display_products.html?products_key=${products_key}'>${products_key}<a>&nbsp&nbsp&nbsp;`);
     }
 }
--- Nhieu_Joey_Assignment3/public/products.js
+++ Nhieu_Joey_Assignment3/public/products.js
@@ -1,5 +1,4 @@
-{ 
-    "Cartoon Keycaps":[ 
+products = [
         {
             "name" : "Artisan Sanrio: Cinnamoroll",
             "price" : 25.15,
@@ -37,8 +36,7 @@
             "image" : "https://i.etsystatic.com/14420193/r/il/3fe0cd/3654602743/il_1588xN.3654602743_o98m.jpg",
             "stock_avl" : 10
         }
-    ],
-    "Floral Keycaps": [ 
+    ,
         {
             "name" : "Artisan Floral 1",
             "price" : 25.15,
@@ -75,44 +73,83 @@
             "image" : "https://i.etsystatic.com/31129726/r/il/28c663/3909679195/il_1588xN.3909679195_5xow.jpg",
             "stock_avl" : 10
         }
-    ],
-    "Keycap Sets": [
-        {
-            "name" : "Blue Keycap Set",
-            "price" : 25.15,
-            "image" : "https://i.etsystatic.com/23346058/r/il/ec5d67/3766668341/il_1588xN.3766668341_l9tj.jpg",
-            "stock_avl" : 10
-        },
-        {
-            "name" : "Yellow Keycap Set",
-            "price" : 25.15,
-            "image" : "https://i.etsystatic.com/33263928/r/il/6c7c15/3894062635/il_1588xN.3894062635_387e.jpg",
-            "stock_avl" : 10
-        },
-        {
-            "name" : "Crystal Keycap Set",
-            "price" : 25.15,
-            "image" : "https://i.etsystatic.com/31503665/r/il/1ccc3d/3713860269/il_1588xN.3713860269_lojl.jpg",
-            "stock_avl" : 10
-        },
-        {
-            "name" : "Pudding Keycap Set",
-            "price" : 25.15,
-            "image" : "https://i.etsystatic.com/33489200/r/il/d5db6d/3728196027/il_1588xN.3728196027_ej3n.jpg",
-            "stock_avl" : 10
-        },
-        {
-            "name" : "Multi-Color Keycap Set",
-            "price" : 25.15,
-            "image" : "https://i.etsystatic.com/33466748/r/il/11f6b6/3735540428/il_1588xN.3735540428_npbs.jpg",
-            "stock_avl" : 10
-        },
-        {
-            "name" : "Themed Keycap Set: Rabbit",
-            "price" : 25.15,
-            "image" : "https://i.etsystatic.com/23346058/r/il/b4f9cf/3332191355/il_1588xN.3332191355_iuy8.jpg",
-            "stock_avl" : 10
-        }
-    ]
+    ,
+    {
+        "name" : "Artisan Floral 1",
+        "price" : 25.15,
+        "image" : "https://i.etsystatic.com/23097501/r/il/c169a6/3617257273/il_1588xN.3617257273_bj66.jpg",
+        "stock_avl" : 10
+    },
+    {
+        "name" : "Artisan Floral 2",
+        "price" : 25.15,
+        "image" : "https://i.etsystatic.com/25641118/r/il/b41242/3832527587/il_1588xN.3832527587_5xbl.jpg",
+        "stock_avl" : 10
+    },
+    {
+        "name" : "Artisan Floral 3",
+        "price" : 25.15,
+        "image" : "https://i.etsystatic.com/25641118/r/il/5693a7/3832536011/il_1588xN.3832536011_1a3k.jpg",
+        "stock_avl" : 10
+    },
+    {
+        "name" : "Artisan Floral 4",
+        "price" : 25.15,
+        "image" : "https://i.etsystatic.com/31503665/r/il/9a56c2/3666248982/il_1588xN.3666248982_ff9o.jpg",
+        "stock_avl" : 10
+    },
+    {
+        "name" : "Artisan Floral 5",
+        "price" : 25.15,
+        "image" : "https://i.etsystatic.com/24746010/r/il/571478/2746964556/il_1588xN.2746964556_u1aq.jpg",
+        "stock_avl" : 10
+    },
+    {
+        "name" : "Artisan Floral 6",
+        "price" : 25.15,
+        "image" : "https://i.etsystatic.com/31129726/r/il/28c663/3909679195/il_1588xN.3909679195_5xow.jpg",
+        "stock_avl" : 10
+    },
 
-}
+    {
+        "name" : "Blue Keycap Set",
+        "price" : 25.15,
+        "image" : "https://i.etsystatic.com/23346058/r/il/ec5d67/3766668341/il_1588xN.3766668341_l9tj.jpg",
+        "stock_avl" : 10
+    },
+    {
+        "name" : "Yellow Keycap Set",
+        "price" : 25.15,
+        "image" : "https://i.etsystatic.com/33263928/r/il/6c7c15/3894062635/il_1588xN.3894062635_387e.jpg",
+        "stock_avl" : 10
+    },
+    {
+        "name" : "Crystal Keycap Set",
+        "price" : 25.15,
+        "image" : "https://i.etsystatic.com/31503665/r/il/1ccc3d/3713860269/il_1588xN.3713860269_lojl.jpg",
+        "stock_avl" : 10
+    },
+    {
+        "name" : "Pudding Keycap Set",
+        "price" : 25.15,
+        "image" : "https://i.etsystatic.com/33489200/r/il/d5db6d/3728196027/il_1588xN.3728196027_ej3n.jpg",
+        "stock_avl" : 10
+    },
+    {
+        "name" : "Multi-Color Keycap Set",
+        "price" : 25.15,
+        "image" : "https://i.etsystatic.com/33466748/r/il/11f6b6/3735540428/il_1588xN.3735540428_npbs.jpg",
+        "stock_avl" : 10
+    },
+    {
+        "name" : "Themed Keycap Set: Rabbit",
+        "price" : 25.15,
+        "image" : "https://i.etsystatic.com/23346058/r/il/b4f9cf/3332191355/il_1588xN.3332191355_iuy8.jpg",
+        "stock_avl" : 10
+    }
+]
+
+
+if(typeof module != 'undefined') {
+    module.exports.products = products;
+  }
